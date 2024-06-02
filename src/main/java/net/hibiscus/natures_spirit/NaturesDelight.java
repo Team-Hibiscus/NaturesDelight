60760cf3dbcd73c9aee275309e5147c8ea02a50f
Eko-byte <85321665+Eko-byte@users.noreply.github.com>
2024-06-01 20:39:16 -0400

    init

---
+++ src/main/java/net/hibiscus/natures_spirit/NaturesDelight.java
@@ -0,0 +1,9 @@
+package net.hibiscus.natures_spirit;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NaturesDelight implements ModInitializer {
+   @Override public void onInitialize() {
+
+   }
+}
---
+++ src/main/java/net/hibiscus/natures_spirit/client/NaturesDelightClient.java
@@ -0,0 +1,9 @@
+package net.hibiscus.natures_spirit.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NaturesDelightClient implements ClientModInitializer {
+   @Override public void onInitializeClient() {
+
+   }
+}
